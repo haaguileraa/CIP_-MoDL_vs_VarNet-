f4ff79ab3867000f99b605c64d73fdf82b6d52aa
haaguileraa <haaguileraa@unal.edu.co>
2023-03-18 02:22:30 +0100

    sbatch run for ESPIRiT from sigpy: it does not work...

---
+++ ESPIRiT/sbatch_run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:a100:1
+#SBATCH --partition=a100
+#SBATCH --job-name=modl_demo
+#SBATCH --time=23:00:00
+#SBATCH --mail-user=hernan.aguilera@fau.de
+#SBATCH --mail-type=ALL
+#
+# do not export environment variables
+#SBATCH --export=NONE
+
+
+
+unset SLURM_EXPORT_ENV
+
+module load cuda
+module load python/3.8-anaconda
+# conda init bash
+eval "$(conda shell.bash hook)"
+conda activate myenv
+
+# mkdir $TMPDIR/$SLURM_JOB_ID
+
+# cp -r /home/vault/iwbi/iwbi005h/fastMRI_data/knee/* $TMPDIR/$SLURM_JOB_ID
+
+python testsig.py #--data_path /home/vault/iwbi/shared/cip22_varnet_modl/brain/ --mode "test" # "test" # "val"
+# rm -r $TMPDIR/$SLURM_JOB_ID
